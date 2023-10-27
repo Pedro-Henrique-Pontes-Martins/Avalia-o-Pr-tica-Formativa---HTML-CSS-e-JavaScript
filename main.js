211dddae9dbcd2bb9935fbef2e95065632350934
30 <pedrosenaitech@gmail.com>
2023-10-27 10:33:19 -0300

    Criação do formulário e ajustes em outras áreas

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var navBar = document.getElementById('navbar');
-var botao1 = document.getElementById('botaoAbrir');
-var botao2 = document.getElementById('botaoFechar');
 function abrirMenu(){
     navBar.style.display = "flex"
     document.getElementById('botaoAbrir').style.display = "none"
